94842a3044199e6fd293c71e66fe17587c4927ed
Avinash parmar <akavinashparmar@gmail.com>
2026-06-23 00:02:52 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const BASE_URL ='https://latest.currency-api.pages.dev/v1/currencies/eur.json'
+const BASE_URL ='https://api.frankfurter.app/latest?from=USD&to=INR'
 
+const dropdowns =document.querySelector(".dropdown select");
 
+let 
 
 
 
